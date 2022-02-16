ecd48f338e29c348fa53fed146457175a2e5c4c7
Vicky Vasilopoulou <vickyvasilopoulou@Vickys-MacBook.local>
2022-02-16 20:36:37 +0000

    feat: add shadows

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -45,8 +45,8 @@ export const Contact = (): JSX.Element => {
         Get In Touch
       </Typography>
       <Typography className={classes.content}>
-        Although I’m not currently looking for any new opportunities, my inbox is always open. Whether you have a
-        question or just want to say hi, I’ll try my best to get back to you!
+        Although I’m not currently looking for any new opportunities, I am always open to hear something new. Whether
+        you have a question or just want to say hi, I’ll try my best to get back to you!
       </Typography>
       <div>
         <Box className={classes.button}>
--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -34,6 +34,8 @@ const useStyles = makeStyles((theme) => ({
     top: 0,
     cursor: 'pointer',
     transition: 'ease 0.5s',
+    boxShadow:
+      'rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset',
     '&:hover': {
       transform: ' translate(0, -20px)',
     },
